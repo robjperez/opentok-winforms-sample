b77011b8d36c2872ffba54fc25a13f9b7eb9ab57
Roberto Perez <robj.perez@gmail.com>
2020-08-24 11:23:13 +0200

    Initial commit

---
+++ OpentokWinForms/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿using OpenTok;
+
+namespace OpentokWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wpfPublisherHost = new System.Windows.Forms.Integration.ElementHost();
+            this.Connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.wpfSubscriberHost = new System.Windows.Forms.Integration.ElementHost();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // wpfPublisherHost
+            // 
+            this.wpfPublisherHost.Location = new System.Drawing.Point(12, 12);
+            this.wpfPublisherHost.Name = "wpfPublisherHost";
+            this.wpfPublisherHost.Size = new System.Drawing.Size(427, 300);
+            this.wpfPublisherHost.TabIndex = 0;
+            this.wpfPublisherHost.Text = "wpfPublisherHost";
+            this.wpfPublisherHost.Child = null;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(355, 390);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(165, 30);
+            this.Connect.TabIndex = 1;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Publisher";
+            // 
+            // wpfSubscriberHost
+            // 
+            this.wpfSubscriberHost.Location = new System.Drawing.Point(445, 12);
+            this.wpfSubscriberHost.Name = "wpfSubscriberHost";
+            this.wpfSubscriberHost.Size = new System.Drawing.Size(427, 300);
+            this.wpfSubscriberHost.TabIndex = 3;
+            this.wpfSubscriberHost.Text = "wpfSubscriberHost";
+            this.wpfSubscriberHost.Child = null;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(624, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Subscriber";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wpfSubscriberHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.wpfPublisherHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost wpfPublisherHost;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Integration.ElementHost wpfSubscriberHost;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OpentokWinForms/Form1.cs
@@ -0,0 +1,197 @@
+﻿using OpenTok;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpentokWinForms
+{
+    public partial class Form1 : Form
+    {
+        public string API_KEY = "";
+        public string TOKEN = "";
+        public string SESSION_ID = "";
+
+        public const int OTC_LOG_LEVEL_ALL = 100;
+
+        VideoCapturer capturer;
+        Session session;
+        Publisher publisher;
+        bool Disconnect = false;
+
+        ConcurrentDictionary<Stream, Subscriber> subscriberByStream = new ConcurrentDictionary<Stream, Subscriber>();
+
+        public void EnableLogging()
+        {
+            otc_logger_func X = (string message) =>
+            {
+                Console.WriteLine(message);
+            };
+            otc_log_enable(OTC_LOG_LEVEL_ALL);
+            otc_log_set_logger_callback(X);
+        }
+
+        // Static interfaces
+        [DllImport("opentok", EntryPoint = "otc_log_set_logger_callback", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void otc_log_set_logger_callback(otc_logger_func logger);
+
+        [DllImport("opentok", EntryPoint = "otc_log_enable", CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void otc_log_enable(int level);
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void otc_logger_func(string message);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            wpfPublisherHost.Child = new OpenTok.VideoRenderer();
+            wpfSubscriberHost.Child = new OpenTok.VideoRenderer();
+
+
+            var cams = VideoCapturer.EnumerateDevices();
+            var selectedcam = cams[0];
+            capturer = selectedcam.CreateVideoCapturer(VideoCapturer.Resolution.High);
+            publisher = new Publisher.Builder(Context.Instance)
+            {
+                Renderer = (IVideoRenderer)wpfPublisherHost.Child,
+                Capturer = capturer
+            }.Build();
+
+            var mics = AudioDevice.EnumerateInputAudioDevices();
+            AudioDevice.SetInputAudioDevice(mics[0]); // Go with first microphone in the list
+
+            session = new Session.Builder(Context.Instance, API_KEY, SESSION_ID).Build();
+
+            session.Connected += Session_Connected;
+            session.Disconnected += Session_Disconnected;
+            session.Error += Session_Error;
+            session.ConnectionCreated += Session_ConnectionCreated;
+            session.StreamReceived += Session_StreamReceived;
+            session.StreamDropped += Session_StreamDropped;
+
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            foreach (var subscriber in subscriberByStream.Values)
+            {
+                subscriber.Dispose();
+            }
+            publisher?.Dispose();
+            capturer?.Dispose();
+            session?.Dispose();
+        }
+
+        private void Session_ConnectionCreated(object sender, Session.ConnectionEventArgs e)
+        {
+            Console.WriteLine("Session connection created:" + e.Connection.Id);
+        }
+
+        private void Session_Error(object sender, Session.ErrorEventArgs e)
+        {
+            Console.WriteLine("Session error:" + e.ErrorCode);
+        }
+
+        private void Session_Disconnected(object sender, EventArgs e)
+        {
+            Console.WriteLine("Session disconnected");
+            subscriberByStream.Clear();
+        }
+
+        // PLEASE NOTE THAT, WHEN USING WINFORMS, OPENTOK CALLBACKS ARE NOT CALLED IN THE MAIN THREAD
+        // ANY CHANGE YOU WANT TO DO IN THE UI HAS TO BE ROUTED TO THE MT
+        private void Session_Connected(object sender, EventArgs e)
+        {
+            Console.WriteLine("Session connected connection id:" + session.Connection.Id);
+            try
+            {
+                session.Publish(publisher);
+            }
+            catch (OpenTokException ex)
+            {
+                Console.WriteLine("OpenTokException " + ex.ToString());
+            }
+
+        }
+
+        private void Session_StreamReceived(object sender, Session.StreamEventArgs e)
+        {
+            Console.WriteLine("Session stream received");
+            Subscriber subscriber = new Subscriber.Builder(Context.Instance, e.Stream)
+            {
+                Renderer = (IVideoRenderer)wpfSubscriberHost.Child
+            }.Build();
+            subscriberByStream.TryAdd(e.Stream, subscriber);
+
+            try
+            {
+                session.Subscribe(subscriber);
+            }
+            catch (OpenTokException ex)
+            {
+                Console.WriteLine("OpenTokException " + ex.ToString());
+            }
+        }
+
+        private void Session_StreamDropped(object sender, Session.StreamEventArgs e)
+        {
+            Console.WriteLine("Session stream dropped");
+            var subscriber = subscriberByStream[e.Stream];
+            if (subscriber != null)
+            {
+                Subscriber outsubs;
+                subscriberByStream.TryRemove(e.Stream, out outsubs);
+
+                try
+                {
+                    session.Unsubscribe(subscriber);
+                }
+                catch (OpenTokException ex)
+                {
+                    Console.WriteLine("OpenTokException " + ex.ToString());
+                }
+            }
+
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            if (Disconnect)
+            {
+                Console.WriteLine("Disconnecting session");
+
+                try
+                {
+                    session.Unpublish(publisher);
+                    session.Disconnect();
+                }
+                catch (OpenTokException ex)
+                {
+                    Console.WriteLine("OpenTokException " + ex.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Connecting session");
+                try
+                {
+                    session.Connect(TOKEN);
+                }
+                catch (OpenTokException ex)
+                {
+                    Console.WriteLine("OpenTokException " + ex.ToString());
+                }
+            }
+            Disconnect = !Disconnect;
+            Connect.Text = Disconnect ? "Disconnect" : "Connect";
+        }
+    }
+}
